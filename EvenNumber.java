8c566de604a32d618f82daafb2c9c3ae213b11df
akinleries <47524901+akinleries@users.noreply.github.com>
2021-02-23 08:10:31 +0100

    assignment

---
+++ EvenNumber.java
@@ -0,0 +1,24 @@
+public class EvenNumber{
+
+		public static void main(String... agrs){
+        int n = 100;
+        
+        System.out.print("The Even from 2 to"+ " " + n +" "+ "are :" );
+         int x = 1;
+        while (x < n){
+       
+        x++;
+        if (x % 3 != 0) {
+        System.out.print(x + " ");
+
+        }
+         
+	} 
+
+
+  }
+
+
+    
+
+ }
---
+++ OddNumber.java
@@ -0,0 +1,23 @@
+public class OddNumber{
+
+	public static void main(String... agrs){
+        int n = 100;
+        
+        System.out.print("The odd from 1 to"+ " " + n +" "+ "are :" );
+         int x = 0;
+        while (x < n){
+       
+        x++;
+        if (x % 2 != 0) {
+        System.out.print(x + " ");
+
+        }
+         
+	} 
+
+
+  }
+
+    
+
+ }
