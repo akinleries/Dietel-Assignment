9f1f432a1766b87c4df23677d34de8f67d7c47d8
akinleries <47524901+akinleries@users.noreply.github.com>
2021-04-12 15:55:35 +0100

    Add files via upload

---
+++ HeroesLectures.java
@@ -0,0 +1,19 @@
+package roughLectures;
+
+public class HeroesLectures {
+    public static void main(String[] args) {
+        checkNumber(6);
+    }
+        public static void checkNumber(int number) {
+            if (number > 0)
+                System.out.println("positive");
+            else if (number < 0)
+                System.out.println("Negative");
+            else if(number == 0)
+                    System.out.println("Zero");
+         }
+
+    }
+
+
+
